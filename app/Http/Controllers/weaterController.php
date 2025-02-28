90f8ced359253a2000901406e33f5830cf54148e
simranjit02 <simranjitsingh58648@gmail.com>
2025-02-28 13:12:00 +0200

    initial commit

---
+++ app/Http/Controllers/weaterController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class weaterController extends Controller
+{
+    public function index(){
+        return view('weater');
+    }
+}
